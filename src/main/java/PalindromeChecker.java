8507cbd6846ab8490e854d4262b27e9e1ccb23b2
sebastiankoster <142959691+sebastiankoster@users.noreply.github.com>
2024-02-20 22:22:48 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,7 +37,7 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  if reverse(word).equals(word)  return true;
   return false;
 }
 public String reverse(String str)
